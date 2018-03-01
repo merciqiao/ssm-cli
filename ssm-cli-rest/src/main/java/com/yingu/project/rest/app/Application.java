bafbe87b77871d13ac72d48526b9fabb5bb2540c
merciqiao <qiaolixue@yingu.com>
2018-03-01 11:00:24 +0800

    1.增加全局controller方法的调用日志记录

--- ssm-cli-rest/src/main/java/com/yingu/project/rest/app/Application.java
+++ ssm-cli-rest/src/main/java/com/yingu/project/rest/app/Application.java
@@ -1,9 +1,12 @@
 package com.yingu.project.rest.app;
 
+import com.yingu.project.rest.config.RequestLog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Properties;
 
@@ -14,9 +17,14 @@ import java.util.Properties;
 		"com.yingu.project"
           })
 @MapperScan("com.yingu.project.persistence.mysql.mapper")
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	// 增加拦截器
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new RequestLog());
 	}
 }
